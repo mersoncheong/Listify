1173c9c9ad96450d16e62aa8352555dfce58e245
shecheeyee <shecheeyee@gmail.com>
2023-03-23 20:59:29 +0800

    interim demo

--- app.py
+++ app.py
@@ -1,204 +1,213 @@
-# ? Cross-origin Resource Sharing - here it allows the view and core applications deployed on different ports to communicate. No need to know anything about it since it's only used once
 from flask_cors import CORS, cross_origin
-# ? Python's built-in library for JSON operations. Here, is used to convert JSON strings into Python dictionaries and vice-versa
 import json
-# ? flask - library used to write REST API endpoints (functions in simple words) to communicate with the client (view) application's interactions
-# ? request - is the default object used in the flask endpoints to get data from the requests
-# ? Response - is the default HTTP Response object, defining the format of the returned data by this api
-from flask import Flask, request, Response
-# ? sqlalchemy is the main library we'll use here to interact with PostgresQL DBMS
+from flask import Flask, redirect, url_for, render_template, request, Response, flash, session
 import sqlalchemy
-# ? Just a class to help while coding by suggesting methods etc. Can be totally removed if wanted, no change
-from typing import Dict
+import psycopg2
+from datetime import date, timedelta
 
+app = Flask(__name__, template_folder='view',
+            static_folder='view/static')
+app.config['SECRET_KEY'] = "secret"
 
-# ? web-based applications written in flask are simply called apps are initialized in this format from the Flask base class. You may see the contents of `__name__` by hovering on it while debugging if you're curious
-app = Flask(__name__)
-
-# ? Just enabling the flask app to be able to communicate with any request source
 CORS(app)
 
-# ? building our `engine` object from a custom configuration string
-# ? for this project, we'll use the default postgres user, on a database called `postgres` deployed on the same machine
+
 YOUR_POSTGRES_PASSWORD = "postgres"
 connection_string = f"postgresql://postgres:{YOUR_POSTGRES_PASSWORD}@localhost/postgres"
-engine = sqlalchemy.create_engine(
-    "postgresql://postgres:postgres@localhost/postgres"
-)
+engine = sqlalchemy.create_engine(connection_string)
 
-# ? `db` - the database (connection) object will be used for executing queries on the connected database named `postgres` in our deployed Postgres DBMS
+
 db = engine.connect()
 
-# ? A dictionary containing
-data_types = {
-    'boolean': 'BOOL',
-    'integer': 'INT',
-    'text': 'TEXT',
-    'time': 'TIME',
-}
 
-# ? @app.get is called a decorator, from the Flask class, converting a simple python function to a REST API endpoint (function)
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 
-@app.get("/table")
-def get_relation():
-    # ? This method returns the contents of a table whose name (table-name) is given in the url `http://localhost:port/table?name=table-name`
-    # ? Below is the default way of parsing the arguments from http url's using flask's request object
-    relation_name = request.args.get('name', default="", type=str)
-    # ? We use try-except statements for exception handling since any wrong query will crash the whole flow
+@app.route("/listing")
+def listings():
+    return marketplace()
+
+
+@app.route("/marketplace")
+def marketplace():
+    if "search" in session:
+        keyword = session["search"]
+        statement = get_search(keyword)
+        second = get_length(keyword)
+        length = db.execute(second).fetchone()[0]
+        session.pop("search", None)
+    elif "filter_date" in session:
+        date = session['filter_date']
+        statement = filter_date(date)
+        session.pop("filter_date",None)
+    else:
+        statement = start_page()
+        length = 100
+
+    if "user" in session:
+        user = session["user"]
+
+    result = db.execute(statement)
+    products = {}
+    container = ["name", "price", "date"]
+    rows = result.fetchall()
+    c = 0
+    for row in rows:
+        my_dict = {}
+        for i in range(3):
+            my_dict[container[i]] = row[i]
+        products[c] = my_dict
+        c += 1
+    return render_template("home.html", user=user, length=length, products=products)
+
+
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return render_template("home.html", user=user)
+    else:
+        flash("Please Login")
+        return redirect(url_for("home"))
+
+
+@app.post("/login")
+def login_into_account():
     try:
-        # ? Statements are built using f-strings - Python's formatted strings
-        # ! Use cursors for better results
-        statement = sqlalchemy.text(f"SELECT * FROM {relation_name};")
-        # ? Results returned by the DBMS after execution are stored into res object defined in sqlalchemy (for reference)
-        res = db.execute(statement)
-        # ? committing the statement writes the db state to the disk; note that we use the concept of rollbacks for safe DB management
-        db.commit()
-        # ? Data is extracted from the res objects by the custom function for each query case
-        # ! Note that you'll have to write custom handling methods for your custom queries
-        data = generate_table_return_result(res)
-        # ? Response object is instantiated with the formatted data and returned with the success code 200
-        return Response(data, 200)
-    except Exception as e:
-        # ? We're rolling back at any case of failure
-        db.rollback()
-        # ? At any error case, the error is returned with the code 403, meaning invalid request
-        # * You may customize it for different exception types, in case you may want
-        return Response(str(e), 403)
+        data = {
+            "name": "registered",
+            "email": request.form.get("email"),
+            "password": request.form.get("password")
+        }
 
-
-# ? a flask decorator listening for POST requests at the url /table-create
-@app.post("/table-create")
-def create_table():
-    # ? request.data returns the binary body of the POST request
-    data = request.data.decode()
-    try:
-        # ? data is converted from stringified JSON to a Python dictionary
-        table = json.loads(data)
-        # ? data, or table, is an object containing keys to define column names and types of the table along with its name
-        statement = generate_create_table_statement(table)
-        # ? the remaining steps are the same
-        db.execute(statement)
-        db.commit()
-        return Response(statement.text)
+        statement = check_login_state(data)
+        result = db.execute(statement)
+        # try:
+        row = result.fetchone()
+        value = row[0]
+        # except:
+        #     flash("Account does not exists")
+        #     return redirect(url_for(home))
+        if value != data["password"]:
+            flash("Wrong Password")
+            return redirect(url_for("home"))
+        else:
+            name = get_username(data["email"])
+            findName = db.execute(name)
+            name = findName.fetchone()[0]
+            session["user"] = name
+            return redirect(url_for("marketplace"))
     except Exception as e:
         db.rollback()
-        return Response(str(e), 403)
+        flash("User does not exists! Please Try Again")
+        return redirect(url_for("home"))
 
 
-@app.post("/table-insert")
+@app.post("/register")
 # ? a flask decorator listening for POST requests at the url /table-insert and handles the entry insertion into the given table/relation
 # * You might wonder why PUT or a similar request header was not used here. Fundamentally, they act as POST. So the code was kept simple here
 def insert_into_table():
-    # ? Steps are common in all of the POST behaviors. Refer to the statement generation for the explanatory
-    data = request.data.decode()
     try:
-        insertion = json.loads(data)
-        statement = generate_insert_table_statement(insertion)
-        db.execute(statement)
-        db.commit()
-        return Response(statement.text)
-    except Exception as e:
-        db.rollback()
-        return Response(str(e), 403)
-
-
-@app.post("/table-update")
-# ? a flask decorator listening for POST requests at the url /table-update and handles the entry updates in the given table/relation
-def update_table():
-    # ? Steps are common in all of the POST behaviors. Refer to the statement generation for the explanatory
-    data = request.data.decode()
-    try:
-        update = json.loads(data)
-        statement = generate_update_table_statement(update)
-        db.execute(statement)
-        db.commit()
-        return Response(statement.text, 200)
-    except Exception as e:
-        db.rollback()
-        return Response(str(e), 403)
-
-
-@app.post("/entry-delete")
-# ? a flask decorator listening for POST requests at the url /entry-delete and handles the entry deletion in the given table/relation
-def delete_row():
-    # ? Steps are common in all of the POST behaviors. Refer to the statement generation for the explanatory
-    data = request.data.decode()
-    try:
-        delete = json.loads(data)
-        statement = generate_delete_statement(delete)
-        db.execute(statement)
-        db.commit()
-        return Response(statement.text)
-    except Exception as e:
-        db.rollback()
-        return Response(str(e), 403)
-
-
-def generate_table_return_result(res):
-    # ? An empty Python list to store the entries/rows/tuples of the relation/table
-    rows = []
-
-    # ? keys of the SELECT query result are the columns/fields of the table/relation
-    columns = list(res.keys())
-
-    # ? Constructing the list of tuples/rows, basically, restructuring the object format
-    for row_number, row in enumerate(res):
-        rows.append({})
-        for column_number, value in enumerate(row):
-            rows[row_number][columns[column_number]] = value
-
-    # ? JSON object with the relation data
-    output = {}
-    output["columns"] = columns  # ? Stores the fields
-    output["rows"] = rows  # ? Stores the tuples
-
-    """
-        The returned object format:
-        {
-            "columns": ["a","b","c"],
-            "rows": [
-                {"a":1,"b":2,"c":3},
-                {"a":4,"b":5,"c":6}
-            ]
+        data = {
+            "name": "registered",
+            "body": {
+                "username": request.form.get("username"),
+                "email": request.form.get("email"),
+                "password": request.form.get("password")
+            }
         }
-    """
-    # ? Returns the stringified JSON object
-    return json.dumps(output)
+        # insertion = json.loads(data)
+        statement = generate_insert_table_statement(data)
+        db.execute(statement)
+        db.commit()
+        flash("Account Creation Sucessful")
+        return redirect(url_for("home"))
+    except Exception as e:
+        db.rollback()
+        flash("Username/Email has been taken. Please try again.")
+        return redirect(url_for("home"))
+        # return Response(str(e), 403)
 
 
-def generate_delete_statement(details: Dict):
-    # ? Fetches the entry id for the table name
-    table_name = details["relationName"]
-    id = details["deletionId"]
-    # ? Generates the deletion query for the given entry with the id
-    statement = f"DELETE FROM {table_name} WHERE id={id};"
+@app.post("/search")
+def search():
+    try:
+        data = request.form.get("search_query")
+        session["search"] = data
+        return redirect(url_for("marketplace"))
+    except:
+        flash("Search Error")
+        return redirect(url_for("marketplace"))
+
+@app.post("/filter")
+def filter_days():
+    data = request.form['dropdown']
+    session['filter_date'] = data
+    return redirect(url_for("marketplace"))
+
+
+
+
+@app.route("/buttonlogout")
+def logout():
+    session.pop("user", None)
+    return render_template("index.html")
+
+
+def check_login_state(insertion):
+    table_name = "registered"
+    email = insertion["email"]
+    password = insertion["password"]
+    statement = f"SELECT password FROM {table_name} WHERE email = '{email}'"
     return sqlalchemy.text(statement)
 
 
-def generate_update_table_statement(update: Dict):
-
-    # ? Fetching the table name, entry/tuple id and the update body
-    table_name = update["name"]
-    id = update["id"]
-    body = update["body"]
-
-    # ? Default for the SQL update statement
-    statement = f"UPDATE {table_name} SET "
-    # ? Constructing column-to-value maps looping
-    for key, value in body.items():
-        statement += f"{key}=\'{value}\',"
-
-    # ?Finalizing the update statement with table and row details and returning
-    statement = statement[:-1]+f" WHERE {table_name}.id={id};"
+def start_page():
+    statement = f"SELECT productname, price, date FROM products ORDER BY popularity DESC LIMIT(100)"
     return sqlalchemy.text(statement)
 
 
-def generate_insert_table_statement(insertion: Dict):
+def get_username(insertion):
+    statement = f"SELECT username FROM registered WHERE email = '{insertion}'"
+    return sqlalchemy.text(statement)
+
+
+def get_search(insertion):
+    statement = f"SELECT productname, price, date FROM products WHERE productname LIKE '%{insertion}%' ORDER BY popularity DESC"
+    return sqlalchemy.text(statement)
+
+
+def get_length(insertion):
+    statement = f"SELECT COUNT(*) FROM products WHERE productname LIKE '%{insertion}%'"
+    return sqlalchemy.text(statement)
+
+def filter_date(value):
+    x = float('inf')
+    if value == "today":
+        x = 0
+    elif value == "past 3 days":
+        x = 3
+    elif value == "past 7 days":
+        x = 7
+    # rem = "0000-00-00"
+    # if value == "today":
+    #     rem = today
+    # elif value == "past 3 days":
+    #     rem = today - timedelta(days = 3)
+    # elif value == "past 7 days":
+    #     rem = today - timedelta(days = 7)
+    # use =rem.strftime("%Y-%m-%d")
+    # statement = f"SELECT productname, price, date FROM products WHERE date >= '{use}'"
+    # return sqlalchemy.text(statement)
+    statement = f"SELECT  productname,price,date FROM producsts WHERE ( date  >= GETDATE() - {x})"
+    
+def generate_insert_table_statement(insertion):
     # ? Fetching table name and the rows/tuples body object from the request
     table_name = insertion["name"]
     body = insertion["body"]
-    valueTypes = insertion["valueTypes"]
+    # valueTypes = insertion["valueTypes"]
 
     # ? Generating the default insert statement template
     statement = f"INSERT INTO {table_name}  "
@@ -208,10 +217,10 @@ def generate_insert_table_statement(insertion: Dict):
     column_values = "("
     for key, value in body.items():
         column_names += (key+",")
-        if valueTypes[key] == "TEXT" or valueTypes[key] == "TIME":
-            column_values += (f"\'{value}\',")
-        else:
-            column_values += (f"{value},")
+        # if valueTypes[key] == "TEXT" or valueTypes[key] == "TIME":
+        #     column_values += (f"\'{value}\',")
+        # else:
+        column_values += (f"'{value}',")
 
     # ? Removing the last default comma
     column_names = column_names[:-1]+")"
@@ -219,37 +228,11 @@ def generate_insert_table_statement(insertion: Dict):
 
     # ? Combining it all into one statement and returning
     #! You may try to expand it to multiple tuple insertion in another method
-    statement = statement + column_names+" VALUES "+column_values+";"
+    statement = statement + column_names+" VALUES " + column_values+";"
     return sqlalchemy.text(statement)
 
 
-def generate_create_table_statement(table: Dict):
-    # ? First key is the name of the table
-    table_name = table["name"]
-    # ? Table body itself is a JSON object mapping field/column names to their values
-    table_body = table["body"]
-    # ? Default table creation template query is extended below. Note that we drop the existing one each time. You might improve this behavior if you will
-    # ! ID is the case of simplicity
-    statement = f"DROP TABLE IF EXISTS {table_name}; CREATE TABLE {table_name} (id serial NOT NULL PRIMARY KEY,"
-    # ? As stated above, column names and types are appended to the creation query from the mapped JSON object
-    for key, value in table_body.items():
-        statement += (f"{key}"+" "+f"{value}"+",")
-    # ? closing the final statement (by removing the last ',' and adding ');' termination and returning it
-    statement = statement[:-1] + ");"
-    return sqlalchemy.text(statement)
-
-# ? This method can be used by waitress-serve CLI 
-def create_app():
-   return app
-
-# ? The port where the debuggable DB management API is served
 PORT = 2222
-# ? Running the flask app on the localhost/0.0.0.0, port 2222
-# ? Note that you may change the port, then update it in the view application too to make it work (don't if you don't have another application occupying it)
+
 if __name__ == "__main__":
     app.run("0.0.0.0", PORT)
-    # ? Uncomment the below lines and comment the above lines below `if __name__ == "__main__":` in order to run on the production server
-    # ? Note that you may have to install waitress running `pip install waitress`
-    # ? If you are willing to use waitress-serve command, please add `/home/sadm/.local/bin` to your ~/.bashrc
-    # from waitress import serve
-    # serve(app, host="0.0.0.0", port=PORT)
