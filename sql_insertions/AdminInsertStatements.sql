58f1190888a8d58089a80ce6208cb755dac98651
Liao Jun Yang <105917409+junyangliao@users.noreply.github.com>
2023-03-31 22:36:19 +0800

    Add files via upload

---
+++ sql_insertions/AdminInsertStatements.sql
@@ -0,0 +1,3 @@
+INSERT INTO Admins (email, username, password, Join_date) VALUES ('shecheeyee@gmail.com', 'shecheeyee', 'password1', '2021-06-09');
+INSERT INTO Admins (email, username, password, Join_date) VALUES ('merson@gmail.com', 'merson', 'password2', '2021-06-09');
+INSERT INTO Admins (email, username, password, Join_date) VALUES ('junyang@gmail.com', 'junyang', 'password3', '2021-06-09');
