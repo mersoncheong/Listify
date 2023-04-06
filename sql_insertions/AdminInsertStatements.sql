b18e7ffdc906b36120375962388c6c38c85042c7
Liao Jun Yang <105917409+junyangliao@users.noreply.github.com>
2023-04-07 02:00:43 +0800

    Delete AdminInsertStatements.sql

--- sql_insertions/AdminInsertStatements.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO Admins (email, username, password, Join_date) VALUES ('shecheeyee@gmail.com', 'shecheeyee', 'password1', '2021-06-09');
-INSERT INTO Admins (email, username, password, Join_date) VALUES ('merson@gmail.com', 'merson', 'password2', '2021-06-09');
-INSERT INTO Admins (email, username, password, Join_date) VALUES ('junyang@gmail.com', 'junyang', 'password3', '2021-06-09');
